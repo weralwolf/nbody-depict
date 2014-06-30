3e390f6167deb919507d6a3f965a8d37bf5026e7
Anatolii Koval <weralwolf@gmail.com>
2014-06-30 09:59:12 +0200

    Increase circle radius on search

--- js/ui.js
+++ js/ui.js
@@ -92,7 +92,7 @@ function InfoCtrl($scope) {
     if ($scope._selected.length) {
       var colormap = document.colormap.maps[document.colormap.current];
       for (var i = 0; i < $scope._selected.length; ++i) {
-        d3.selectAll('#E' + $scope._selected[i]).style('fill', colormap[$scope._selected[i]]).style('fill-opacity', 0.6);
+        d3.selectAll('#E' + $scope._selected[i]).style('fill', colormap[$scope._selected[i]]).style('fill-opacity', 0.6).attr('r', 10);
       }
     }
 
@@ -115,7 +115,7 @@ function InfoCtrl($scope) {
     }
 
     if ($scope._selected.length) {
-      $scope._make_selection().style('fill', 'yellow').style('fill-opacity', 1.);
+      $scope._make_selection().style('fill', 'yellow').style('fill-opacity', 1.).attr('r', 20);
     }
   }
 }
